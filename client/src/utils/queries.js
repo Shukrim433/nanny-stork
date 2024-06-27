5c01b60612a5080531c2eef35845e8b93c4079c2
Shukri <150862418+Shukrim433@users.noreply.github.com>
2024-06-27 22:09:52 +0100

    created ADD_POST mutation and QUERY_ME, QUERY_POSTS queries

---
+++ client/src/utils/queries.js
@@ -0,0 +1,31 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      posts {
+        _id
+        postText
+        postAuthor
+        createdAt
+      }
+    }
+  }
+`;
+
+// getPosts - name of the query
+// posts - name of resolver function to fetch an array of all posts
+// {} - what is returned by this query (an array of post objects)
+export const QUERY_POSTS = gql`
+  query getPosts { 
+    posts {
+      _id
+      postText
+      postAuthor
+      createdAt
+    }
+  }
+`;
