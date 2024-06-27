97b5881637bc1fe9fa8447cfba1da16ccb2342bd
ANUDT <152813453+ANUDT@users.noreply.github.com>
2024-06-27 19:30:03 +0100

    footer created

---
+++ client/src/components/Footer/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <footer>
+        <p className="Ftext">Copyright Hustler HQ 2024 ©</p>
+        </footer>
+    );
+}
