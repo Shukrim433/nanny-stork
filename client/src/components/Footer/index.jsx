a655faab80913b64a23df681bc1c70932882e975
ANUDT <152813453+ANUDT@users.noreply.github.com>
2024-06-27 19:47:33 +0100

    Further footer changes

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -3,7 +3,22 @@ import React from "react";
 export default function Footer() {
     return (
         <footer>
-        <p className="text">Copyright Hustler HQ 2024 ©</p>
-        </footer>
+        <div class="footer-container">
+            <div class="footer-section">
+                <h4>About Us</h4>
+                <p>Nanny Stork, AI Powered solutions for newly parents.</p>
+            </div>
+            <div class="footer-section">
+                <h4>Links</h4>
+                <ul>
+                <li><a href="#">Homepage</a></li>
+                <li><a href="#">More About Us</a></li>
+                <li><a href="#">Other Services</a></li>
+                <li><a href="#">Contact Details </a></li>
+            </ul>
+        </div>
+    <p className="text">Copyright Hustler HQ 2024 ©</p>
+    </footer>
     );
 }
+
