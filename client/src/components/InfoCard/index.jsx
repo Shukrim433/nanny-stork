a92eb027c25f6d08fb5ca9f5e82437f0c81ecc84
LWSE13 <leewheeler888@gmail.com>
2024-06-28 03:12:40 +0100

    added cards to homepage that prompt you to sign up when clicked

--- client/src/components/InfoCard/index.jsx
+++ client/src/components/InfoCard/index.jsx
@@ -6,14 +6,16 @@ import {
     Typography,
     Button,
   } from "@material-tailwind/react";
+  import { Link } from 'react-router-dom';
    
-  export default function InfoCard({ imageURL, title, description, btnText }) {
+  export default function InfoCard({ imageURL, title, description, btnText, imgClass }) {
     return (
       <Card className="mt-6 w-96">
         <CardHeader color="blue-gray" className="relative h-56">
           <img
             src={imageURL}
             alt="card-image"
+            className={imgClass}
           />
         </CardHeader>
         <CardBody>
@@ -25,7 +27,9 @@ import {
           </Typography>
         </CardBody>
         <CardFooter className="pt-0">
+          <Link to="/signUp">
           <Button>{btnText}</Button>
+          </Link>
         </CardFooter>
       </Card>
     );
