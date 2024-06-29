ac4ec58f06afb178a6d0fd1ca0f7aacc84930fd1
Shukri <150862418+Shukrim433@users.noreply.github.com>
2024-06-29 22:22:06 +0100

    when user clicks of username on post it takes you to /profiles/:username unless the username = logged in user, in that case it takes you to /me

--- client/src/components/PostList/index.jsx
+++ client/src/components/PostList/index.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import './postList.css';
+import Auth from '../../utils/auth';
 
 const PostList = ({ posts, title}) => {
     
@@ -12,25 +13,25 @@ const PostList = ({ posts, title}) => {
             <h2>{title}</h2>
             {posts &&
                 posts.map((post) => (
-                    <div key={post._id} className="single-post">
-                        <h4>
-                            {/* click on username it takes u to the user's profile page */}
-                            <Link to={`/profiles/${post.postAuthor}`} > 
+                    <div key={post._id} className="single-post-card">
+                        <p>
+                            {/* click on username it takes u to the user's profile page, if the username is the logged in user's then ur redirect to /me */}
+                            <Link to={ post.postAuthor === Auth.getProfile().authenticatedPerson.username ? (`/me`) : (`/profiles/${post.postAuthor}`)} > 
                             {post.postAuthor}
-                            </Link> <br/>
-                            {/* click on post title it take you to the posts individual page */}
-                            <span>
-                                <Link to={`/posts/${post._id}`}>
-                                {post.postTitle}
-                                </Link>
-                            </span>
-                        </h4>
-                        <div className="single-post-body">
-                            <p> {post.postText} </p>
+                            </Link>
+                        </p> 
+                        {/* click on post title it take you to the posts individual page */}
+                        <h2>
+                            <Link to={`/posts/${post._id}`}>
+                            {post.postTitle}
+                            </Link>
+                        </h2>
+                        
+                        <div>
+                            <p>{post.postText}</p>
                         </div>
-                        <div className="single-post-footer">
-                            <span> posted on {post.createdAt} </span>
-                        </div>
+
+                        <p> posted on {post.createdAt}</p>
                     </div>
                 ))
             }
