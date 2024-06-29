f57fc9b9344b028d22b3c74235f75974070e8790
Shukri <150862418+Shukrim433@users.noreply.github.com>
2024-06-29 22:22:33 +0100

    OtherProfile page for non logged in users

---
+++ client/src/pages/OtherProfile.jsx
@@ -0,0 +1,41 @@
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+
+import { QUERY_USER } from '../utils/queries';
+import PostsList from '../components/PostList'
+
+const OtherProfile = () => {
+    // use useParams() hook to get the value of the route param in the url `/profiles/:username`
+    const { username } = useParams()
+
+    // QUERY_USER query
+    const { loading, data } = useQuery(QUERY_USER, {
+    // pass the URL parameter(the username) to the query
+        variables: { username: username }
+    })
+
+    // if the "data" returned by the query is falsy, post = empty object{}
+    const user = data?.user || {}
+
+    if (loading) {
+        return <div>Loading...</div>
+    }
+
+    return (
+        <div>
+            <h1>{user.username}'s Profile</h1>
+
+            <div>
+                <h2>{user.username}'s posts:</h2>
+                <PostsList
+                posts={user.posts}
+                />
+            </div>
+        </div>
+    )
+    
+
+}
+
+
+export default OtherProfile
