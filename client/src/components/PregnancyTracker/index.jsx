46bbcb72a7360b3e28691191a946d0368a3eca67
Shukri <150862418+Shukrim433@users.noreply.github.com>
2024-07-02 23:42:40 +0100

    gets the pregTracker associated w the logged in user and displays it + edit btn to update tracker

--- client/src/components/PregnancyTracker/index.jsx
+++ client/src/components/PregnancyTracker/index.jsx
@@ -1,22 +1,63 @@
-import { useQuery } from '@apollo/client';
-import { QUERY_PREGNANCY_TRACKER } from '../../utils/queries'
+import { useState, useEffect } from 'react';
+import { useQuery } from "@apollo/client";
+import { QUERY_USER } from "../../utils/queries";
+import Auth from "../../utils/auth";
+import PregnancyTrackerForm from '../PregnancyTrackerForm'
+import './tracker.css';
 
-const  PregnancyTracker = ({ trackerId }) => {
-    // QUERY_PREGNANCY_TRACKER query
-    const { loading, data } = useQuery(QUERY_PREGNANCY_TRACKER, {
-    variables: { trackerId: trackerId }
-    });
-    const tracker = data?.pregnancyTracker || {}
+const PregnancyTracker = () => {
+  const [isEditing, setIsEditing] = useState(false)
+  // QUERY_USER_TRACKER query
+  const { loading, data, error } = useQuery(QUERY_USER, {
+        variables: { username:  Auth.getProfile().authenticatedPerson.username }
+    })
 
-    if (loading ) {
-        return <div>Loading...</div>
-    }
-    return (
-        <div>
-            pregnancy tracker:
-            {tracker.stage}
-        </div>
-    )
-}
+  console.log(data, loading, error, "tracker data");
+  const pregnancyTracker = data?.user || {}
+
+  const formatUnixTimestamp = (timestamp) => {
+    if (!timestamp) return '';
+    const timestampInt = parseInt(timestamp, 10);
+    const date = timestampInt.toString().length === 13 ? new Date(timestampInt) : new Date(timestampInt * 1000);
+    return date.toLocaleDateString();
+  };
+
+  const trackerData = {
+            stage: pregnancyTracker.tracker.stage,
+            dueDate: formatUnixTimestamp(pregnancyTracker.tracker.dueDate) || '',
+            birthDate:formatUnixTimestamp( pregnancyTracker.tracker.birthDate) || ''
+  }
+
+  const handleEditClick = () => {
+    setIsEditing(true);
+  }
+
+  return (
+  <div className="tracker-container">
+    {!isEditing ? (
+      <>
+      <p>
+      {trackerData.stage === 'pregnancy' ? (
+        <>
+        <p>You are currently pregnant! </p>
+        {trackerData.dueDate ? (<p>Your due date is: {trackerData.dueDate}</p>) : ('')}
+        </>
+        ) : (
+          <>
+        <p>You are currently postpartum! </p>
+        {trackerData.birthDate ? (<p>Your birth date was: {trackerData.birthDate}</p>) : ('') }
+        </>
+        )}
+      </p>
+      
+      <button onClick={handleEditClick}>Edit Tracker</button>
+      </>
+    ) : (
+      <PregnancyTrackerForm initialData={trackerData}  trackerId={pregnancyTracker.tracker._id}/>
+    )}
+    
+  </div>
+)
+};
 
 export default PregnancyTracker;
