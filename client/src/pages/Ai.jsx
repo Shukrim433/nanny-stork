2265dc1b78759fcd0fd26c2f7c8da64e9592e65b
LWSE13 <leewheeler888@gmail.com>
2024-07-04 05:27:06 +0100

    refactored ai page to use material tailwind

--- client/src/pages/Ai.jsx
+++ client/src/pages/Ai.jsx
@@ -1,46 +1,46 @@
-import { useState } from 'react'
-import SearchInput from '../components/SearchInput'
-import Credits from '../components/Credits'
-import HomeOptions from '../components/HomeOptions'
-import TypeWriterText from '../components/TypeWriterTextEffect'
+import { useState } from 'react';
+import SearchInput from '../components/SearchInput';
+import Credits from '../components/Credits';
+import HomeOptions from '../components/HomeOptions';
+import TypeWriterText from '../components/TypeWriterTextEffect';
 
-//import answers from './answers/fl1.json'
+//import answers from './answers/fl1.json';
 
 export default function Ai() {
 
-  const [search, setSearch] = useState('')
-  const [showMore, setShowMore] = useState(false)
+  const [search, setSearch] = useState('');
+  const [showMore, setShowMore] = useState(false);
 
   const onSearch = (searchText) => {
     setSearch(searchText);
   }
 
   return (
-    <main className="flex flex-col items-center justify-center w-full h-screen bg-gray-800">
-      <header className="flex items-center justify-center w-1/2 p-10 text-gray-800 bg-gray-800 dark:text-gray-100">
-        <h1 className="text-4xl font-bold">Chat GPT</h1>
+    <main className="flex flex-col items-center justify-center w-full h-screen bg-gray-900">
+      <header className="flex items-center justify-center w-1/2 py-6 text-white bg-gray-900">
+        <h1 className="text-4xl font-bold">Nanny Stork</h1>
       </header>
-      <section className="flex flex-col items-center justify-center w-1/2 p-10 text-gray-800 bg-gray-800 dark:text-gray-100">
+      <section className="flex flex-col items-center justify-center w-1/2 py-6 text-white bg-gray-900">
 
         {search.length === 0 ?
           <HomeOptions />
           :
           <>
-            <div className='flex items-center justify-center w-full mt-20'>
+            <div className='flex items-center justify-center w-full mt-10'>
               <TypeWriterText text={answers.answer1} onTextCompleted={setShowMore} />
             </div>
             {showMore &&
-              <div className='flex items-center justify-center w-full mt-20'>
+              <div className='flex items-center justify-center w-full mt-10'>
                 <TypeWriterText text={answers.answer2} onTextCompleted={setShowMore} />
               </div>
             }
           </>}
-        <div className='flex items-center justify-center w-full mt-20'>
+        <div className='flex items-center justify-center w-full mt-10'>
           <SearchInput onSearch={onSearch} />
         </div>
 
       </section>
-      <footer className="flex items-center justify-center w-1/2 p-10 text-gray-800 bg-gray-800 dark:text-gray-100">
+      <footer className="flex items-center justify-center w-1/2 py-6 text-white bg-gray-900">
         <Credits />
       </footer>
     </main>
