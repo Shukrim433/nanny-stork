f9cd3d112f992d1ce46b39e584fe48194b123367
LWSE13 <leewheeler888@gmail.com>
2024-07-04 20:29:05 +0100

    refactored search bar into material tailwind template

--- client/src/components/SearchInput/index.jsx
+++ client/src/components/SearchInput/index.jsx
@@ -29,10 +29,10 @@ const SearchInput = ({ onSearch }) => {
                 <Button
     style={{ background: '#f48fb1' }}
     size="sm"
-    className="mt-2 w-1/6 flex justify-center items-center" // Ensure flexbox centering
+    className="mt-2 w-1/6 flex justify-center items-center" 
     onClick={onClickSearch}
 >
-    <svg className='' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4"> {/* Adjusted size with w-4 h-4 */}
+    <svg className='' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4"> 
         <path stroke-linecap="round" stroke-linejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
     </svg>
 </Button>
