39ee073df0b6a6aaad05d220a81c3642ef35ba31
LWSE13 <leewheeler888@gmail.com>
2024-07-06 00:46:54 +0100

    finished sidebar. hidden unnecesary links on mobile and integrated mobile sidebar THIS MAY BE MY LAST COMMIT WOOHOO ;)

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -25,8 +25,8 @@ export default function Header() {
   return (
     <>
       <header className="flex justify-center items-center flex-col">
-        <div className="header-top flex flex-row justify-between items-center w-3/4 ml-1">
-          <div className="header-buttons-left flex justify-center items-center ml-5">
+        <div className="header-top flex flex-row justify-between items-center md:w-3/4 lg:ml-1">
+          <div className="header-buttons-left md:flex justify-center items-center ml-5 hidden">
             <Button 
             onClick={toggleTheme}
             className={themeStyles}>
@@ -34,22 +34,27 @@ export default function Header() {
                 <path strokeLinecap="round" strokeLinejoin="round" d="M9.53 16.122a3 3 0 0 0-5.78 1.128 2.25 2.25 0 0 1-2.4 2.245 4.5 4.5 0 0 0 8.4-2.245c0-.399-.078-.78-.22-1.128Zm0 0a15.998 15.998 0 0 0 3.388-1.62m-5.043-.025a15.994 15.994 0 0 1 1.622-3.395m3.42 3.42a15.995 15.995 0 0 0 4.764-4.648l3.876-5.814a1.151 1.151 0 0 0-1.597-1.597L14.146 6.32a15.996 15.996 0 0 0-4.649 4.763m3.42 3.42a6.776 6.776 0 0 0-3.42-3.42" />
               </svg>
             </Button>
-            <Sidebar />
           </div>
-          <img src="/images/ns-logo.png" alt="nanny-stork-logo" className="w-56 lg:ml-12" />
+          <img src="/images/ns-logo.png" alt="nanny-stork-logo" className="w-56 lg:ml-12 " />
           <div className="header-buttons-right flex justify-center items-center">
             {Auth.loggedIn() ? (
               <>
-                <Link to="/me">
+              <div className="md:flex flex-row hidden">
+                <button>
+                <Link to="/me"
+                className="mt-1">
                   profile
                 </Link>
-                <p className="text-xl mx-2 text-gray-500">|</p>
+                </button>
+                <p className="text-xl mx-2 text-gray-500 ">|</p>
                 <button onClick={logout}>
                   Logout
                 </button>
+                </div>
               </>
             ) : (
               <>
+              <div className="md:flex flex-row hidden">
                 <Link to="/login" className="text-black hover:underline">
                   Login
                 </Link>
@@ -57,18 +62,21 @@ export default function Header() {
                 <Link to="/signup" className="text-black hover:underline">
                   Signup
                 </Link>
+                </div>
               </>
             )}
           </div>
         </div>
-        <nav className="w-2/3">
+        <nav className="md:w-2/3">
+        <div className="md:hidden"><Sidebar/></div>
           <ul className="navbar flex justify-between lg:ml-9">
-            <li className="navLink text-lg font-thin hover:text-gray-700"><Link to="/">HOME</Link></li>
-            <li className="navLink text-lg font-thin hover:text-gray-700"><Link to="/posts">POSTS</Link></li>
+            <li className="navLink text-lg font-thin hover:text-gray-700 md:block hidden"><Link to="/">HOME</Link></li>
+            <li className="navLink text-lg font-thin hover:text-gray-700 md:block hidden"><Link to="/posts">POSTS</Link></li>
+
             {Auth.loggedIn() ? (
               <>
-                <li className="navLink text-lg font-thin hover:text-gray-700"><Link to="/about">ABOUT US</Link></li>
-                <li className="navLink text-lg font-thin hover:text-gray-700"><Link to="/newPost">NEW POST</Link></li>
+                <li className="navLink text-lg font-thin hover:text-gray-700 md:block hidden"><Link to="/about">ABOUT US</Link></li>
+                <li className="navLink text-lg font-thin hover:text-gray-700 md:block hidden"><Link to="/newPost">NEW POST</Link></li>
               </>
             ) : null}
           </ul>
