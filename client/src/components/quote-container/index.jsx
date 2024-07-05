e755ac4b24412b3d9e1c0ae2e1c25a6c7e392244
LWSE13 <leewheeler888@gmail.com>
2024-07-05 05:36:31 +0100

    made baby quote container into seperate component for plans to include on every page

---
+++ client/src/components/quote-container/index.jsx
@@ -0,0 +1,7 @@
+<div className="baby-quote-container w-full bg-pink-200 flex items-center justify-center mb-11 mt-5 py-20">
+<div className="baby-quote max-w-xl text-center">
+  <p className="baby-quote-text text-xl font-semibold text-white">
+    "Waah I'm a baby. And I be cryin and shit" - Lee Wheeler: 13/05/2001
+  </p>
+</div>
+</div>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -8,13 +8,6 @@ export default function Home() {
   return (
     <>
       <div className="page-container flex flex-col min-h-screen ">
-        <div className="baby-quote-container w-full bg-pink-200 flex items-center justify-center mb-11 mt-5 py-20">
-          <div className="baby-quote max-w-xl text-center">
-            <p className="baby-quote-text text-xl font-semibold text-white">
-              "Waah I'm a baby. And I be cryin and shit" - Lee Wheeler: 13/05/2001
-            </p>
-          </div>
-        </div>
         <div className="info-allignment flex justify-center gap-12">
             <InfoCard
             imageURL="images/stork-playing.png"
