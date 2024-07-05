759866e7055da1dac1ff0ae4144a531b361f73cb
LWSE13 <leewheeler888@gmail.com>
2024-07-05 23:46:00 +0100

    added theme changer into sidebar

--- client/src/components/Sidebar/index.jsx
+++ client/src/components/Sidebar/index.jsx
@@ -1,37 +1,112 @@
-import React, { useState } from "react";
-import { Drawer, IconButton, List, ListItem, Typography, Switch } from "@material-tailwind/react";
-import { Bars3Icon, XMarkIcon, UserCircleIcon, MoonIcon, SunIcon } from "@heroicons/react/24/outline";
-
+import React from "react";
+import { useTheme } from "../../utils/ThemeContext";
+import {
+  IconButton,
+  Typography,
+  List,
+  ListItem,
+  ListItemPrefix,
+  ListItemSuffix,
+  Chip,
+  Accordion,
+  AccordionHeader,
+  AccordionBody,
+  Alert,
+  Input,
+  Drawer,
+  Card,
+  Button,
+} from "@material-tailwind/react";
+import {
+  PresentationChartBarIcon,
+  ShoppingBagIcon,
+  UserCircleIcon,
+  Cog6ToothIcon,
+  InboxIcon,
+  PowerIcon,
+} from "@heroicons/react/24/solid";
+import {
+  ChevronRightIcon,
+  ChevronDownIcon,
+  CubeTransparentIcon,
+  MagnifyingGlassIcon,
+  Bars3Icon,
+  XMarkIcon,
+} from "@heroicons/react/24/outline";
+ 
 export default function Sidebar() {
-  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-  const [isDarkTheme, setIsDarkTheme] = useState(false);
-
-  const toggleDrawer = () => setIsDrawerOpen(!isDrawerOpen);
-  const toggleTheme = () => setIsDarkTheme(!isDarkTheme);
-
+    const { pinkTheme, toggleTheme } = useTheme();
+    const themeStyles = pinkTheme  // if pinkTheme is set to true...
+  ? "rounded-full border border-2 border-black bg-pink-100 px-2 py-1 hover:bg-blue-100 transition duration-300 ease-in-out"// this is the pinl theme styling  
+  : "rounded-full border border-2 border-black bg-blue-100 px-2 py-1 hover:bg-pink-100 transition duration-300 ease-in-out"
+  const [open, setOpen] = React.useState(0);
+  const [openAlert, setOpenAlert] = React.useState(true);
+  const [isDrawerOpen, setIsDrawerOpen] = React.useState(false);
+ 
+  const handleOpen = (value) => {
+    setOpen(open === value ? 0 : value);
+  };
+ 
+  const openDrawer = () => setIsDrawerOpen(true);
+  const closeDrawer = () => setIsDrawerOpen(false);
+ 
   return (
     <>
-      <IconButton variant="text" size="lg" onClick={toggleDrawer}>
-        {isDrawerOpen ? <XMarkIcon className="h-6 w-6" /> : <Bars3Icon className="h-6 w-6" />}
+      <IconButton variant="text" size="lg" onClick={openDrawer}>
+        {isDrawerOpen ? (
+          <XMarkIcon className="h-8 w-8 stroke-2" />
+        ) : (
+          <Bars3Icon className="h-8 w-8 stroke-2" />
+        )}
       </IconButton>
-      <Drawer open={isDrawerOpen} onClose={toggleDrawer} size="w-64">
-        <List>
-          <ListItem>
-            <UserCircleIcon className="h-5 w-5 mr-2" />
-            Profile
-          </ListItem>
-          {/* Add more navigation links here */}
-        </List>
-        <div className="absolute bottom-0 w-full p-4">
-          <div className="flex items-center justify-between">
-            <Typography>Dark Theme</Typography>
-            <Switch checked={isDarkTheme} onChange={toggleTheme} />
+      <Drawer open={isDrawerOpen} onClose={closeDrawer}>
+        <Card
+          color="transparent"
+          shadow={false}
+          className="h-[calc(100vh-2rem)] w-full p-4"
+        >
+          <div className="mb-2 flex items-center gap-4 p-4">
+          <Button 
+            onClick={toggleTheme}
+            className={themeStyles}>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M9.53 16.122a3 3 0 0 0-5.78 1.128 2.25 2.25 0 0 1-2.4 2.245 4.5 4.5 0 0 0 8.4-2.245c0-.399-.078-.78-.22-1.128Zm0 0a15.998 15.998 0 0 0 3.388-1.62m-5.043-.025a15.994 15.994 0 0 1 1.622-3.395m3.42 3.42a15.995 15.995 0 0 0 4.764-4.648l3.876-5.814a1.151 1.151 0 0 0-1.597-1.597L14.146 6.32a15.996 15.996 0 0 0-4.649 4.763m3.42 3.42a6.776 6.776 0 0 0-3.42-3.42" />
+              </svg>
+            </Button>
+            <Typography variant="h5" color="blue-gray">
+              Sidebar
+            </Typography>
           </div>
-          <div className="flex justify-between mt-4">
-            <button className="py-2 px-4 bg-blue-500 text-white rounded">Log In</button>
-            <button className="py-2 px-4 bg-gray-500 text-white rounded">Log Out</button>
+          <div className="p-2">
+            <Input
+              icon={<MagnifyingGlassIcon className="h-5 w-5" />}
+              label="Search"
+            />
           </div>
-        </div>
+          <List>
+           
+            <hr className="my-2 border-blue-gray-50" />
+            
+            <ListItem>
+              <ListItemPrefix>
+                <UserCircleIcon className="h-5 w-5" />
+              </ListItemPrefix>
+              Profile
+            </ListItem>
+            <ListItem>
+              <ListItemPrefix>
+                <Cog6ToothIcon className="h-5 w-5" />
+              </ListItemPrefix>
+              Settings
+            </ListItem>
+            <ListItem>
+              <ListItemPrefix>
+                <PowerIcon className="h-5 w-5" />
+              </ListItemPrefix>
+              Log Out
+            </ListItem>
+          </List>
+        </Card>
       </Drawer>
     </>
   );
